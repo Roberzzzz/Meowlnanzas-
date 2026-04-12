368208329c0f2fbc2aa03e09c028d4d0fc079964
Robert Montero <rema182006@gmail.com>
2026-04-12 10:43:34 -0400

    Agregado el jar de pdfs y resuelto el conflicto de imports

--- JavaPynanzas/src/javapynanzas/GenPdf.java
+++ JavaPynanzas/src/javapynanzas/GenPdf.java
@@ -1,10 +1,19 @@
 package javapynanzas;
 
-import com.itextpdf.text.*;
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Chunk;
+import com.itextpdf.text.Document; 
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;     
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.Rectangle;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 import com.itextpdf.text.pdf.draw.LineSeparator;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.awt.Desktop;
